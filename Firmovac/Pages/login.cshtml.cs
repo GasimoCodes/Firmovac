d75a38484dec02fbc23c470cf5d2cdeb8729ee5c
Michal-Sivicek <73881006+Michal-Sivicek@users.noreply.github.com>
2023-02-16 16:17:41 +0100

    login form
    Přidanej login formulář pod odkazem na hlavní stránce "přihlášení"

---
+++ Firmovac/Pages/login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Firmovac.Pages
+{
+    public class registerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
