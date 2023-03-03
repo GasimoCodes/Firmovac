8fe21df9778b3538915d2d778334e801acfba5d8
NeightN <neightn7@gmail.com>
2023-03-03 14:27:24 +0100

    checkbox update

--- Firmovac/wwwroot/js/site.js
+++ Firmovac/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function toggle(source) {
+    checkboxes = document.getElementsByName('foo');
+    for (var i = 0, n = checkboxes.length; i < n; i++) {
+        checkboxes[i].checked = source.checked;
+    }
+}
