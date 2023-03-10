825cebecef560611d50291de9bf485f4bb425c05
NeightN <neightn7@gmail.com>
2023-03-10 14:25:05 +0100

    table fixed

--- Firmovac/wwwroot/js/site.js
+++ Firmovac/wwwroot/js/site.js
@@ -9,20 +9,3 @@ function toggle(source) {
         checkboxes[i].checked = source.checked;
     }
 }
-
-function searchFunction() {
-    var input, filter, table, tr, name, i, txtValue;
-    input = document.getElementById("searchInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("searchTB");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        name = tr[i].getElementsByTagName("td")[1];
-        txtValue = name.textContent || name.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            tr[i].style.display = "";
-        } else {
-            tr[i].style.display = "none";
-        }
-    }
-}
