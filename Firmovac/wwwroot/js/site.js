36f4b08a8b8d3c6a518229b2855768051301377f
NeightN <neightn7@gmail.com>
2023-03-24 00:54:00 +0100

    added script

--- Firmovac/wwwroot/js/site.js
+++ Firmovac/wwwroot/js/site.js
@@ -3,9 +3,49 @@
 
 // Write your JavaScript code.
 
+//All checkboxes are checked when the first checkbox is checked
 function toggle(source) {
     checkboxes = document.getElementsByName('foo');
     for (var i = 0, n = checkboxes.length; i < n; i++) {
         checkboxes[i].checked = source.checked;
     }
 }
+
+
+// Získání checkboxů v tabulce
+var checkboxes = document.querySelectorAll('#searchTB input[type=checkbox]');
+
+// Získání elementu pro zobrazení názvů firem v modálním okně
+var selectedRows = document.querySelector('#selectedRows');
+
+// Funkce pro zobrazení názvů firem v modálním okně
+function showSelectedRows() {
+    var names = [];
+    // Projdeme všechny checkboxy a pokud jsou zaškrtnuté, uložíme název firmy
+    checkboxes.forEach(function (checkbox) {
+        if (checkbox.checked) {
+            var name = checkbox.closest('tr').querySelector('.firm-name').textContent;
+            names.push(name);
+        }
+    });
+
+    // Zobrazíme názvy firem v modálním okně
+    const selectedRows = document.querySelector('#selectedRows');
+
+    // Odstraníme všechny děti elementu selectedRows
+    while (selectedRows.firstChild) {
+        selectedRows.removeChild(selectedRows.firstChild);
+    }
+
+    // Pokud jsou nějaké vybrané řádky, zobrazíme je jako tagy
+    if (names.length > 0) {
+        for (let i = 0; i < names.length; i++) {
+            const tag = document.createElement('span');
+            tag.classList.add('badge','rounded-pill','bg-dark','p-3', 'm-1');
+            tag.textContent = names[i];
+            selectedRows.appendChild(tag);
+        }
+    }
+}
+
+
