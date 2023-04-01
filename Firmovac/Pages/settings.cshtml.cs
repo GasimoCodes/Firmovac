c99f342bbf971974a905e5d41855ecaebb65f508
Gasimo <prokop.svacina@gmail.com>
2023-04-01 11:28:37 +0200

    Přidána konfigurační stránka + ukládání

--- Firmovac/Pages/settings.cshtml.cs
+++ Firmovac/Pages/settings.cshtml.cs
@@ -7,13 +7,38 @@ namespace Firmovac.Pages
 {
     public class SettingsModel : PageModel
     {
+        // Properties 
 
         [BindProperty]
 		[Display(Name = "SMTP Adresa")]
 		public string SMTP_Address { get; set; }
+
+        [BindProperty]
+        [Display(Name = "SMTP Heslo")]
+        public string SMTP_Password { get; set; }
+
+        [BindProperty]
+        [Display(Name = "Admin Email")]
+        public string Admin_Mail { get; set; }
+
+
+
         public void OnGet()
         {
-
+            SMTP_Address = Utils.siteConfig.SMTP_Address;
+            SMTP_Password= Utils.siteConfig.SMTP_Password;
+            Admin_Mail = Utils.siteConfig.Admin_Email;
         }
+
+        [HttpPost]
+        public void OnPost()
+        {
+            Utils.siteConfig.SMTP_Address = SMTP_Address;
+            Utils.siteConfig.SMTP_Password = SMTP_Password;
+            Utils.siteConfig.Admin_Email = Admin_Mail;
+
+            Utils.SaveConfig();
+        }
+
     }
 }
