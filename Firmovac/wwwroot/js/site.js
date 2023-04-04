26a9a299b65f13c1066ca775cf820b2ba84c6506
NeightN <neightn7@gmail.com>
2023-04-04 11:32:24 +0200

    add contact

--- Firmovac/wwwroot/js/site.js
+++ Firmovac/wwwroot/js/site.js
@@ -95,7 +95,44 @@ function postIdFirm(arg) {
     console.log(data);
     firmyIds.length = 0;
 
-
-
-
 }
+
+
+function addNewAccordion() {
+    const card = document.getElementById('accordionCard');
+    const newIndex = card.children.length;
+
+    const newAccordion = `
+    <div class="accordion mt-1 w-100" id="accord_contact_${newIndex}">
+      <div class="accordion-item rounded-4 shadow-sm p-1">
+        <button class="own-p-tag px-3  accordion-button rounded-3 btn bg-transparent shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#accord_contactItem_${newIndex}" aria-expanded="false" aria-controls="collapseOne">
+          <div class="flex-row align-items-center d-inline-flex">
+            <a class="btn"><i class="bi bi-x-lg"></i></a>
+            <span>Kontakt ${newIndex + 1}</span>
+          </div>
+        </button>
+        <div id="accord_contactItem_${newIndex}" class="accordion-collapse rounded-4 collapse p-3" aria-labelledby="headingOne" data-bs-parent="#accord_contact_${newIndex}">
+          <input name="Kontakty[${newIndex}].Id" type="hidden" value="0">
+          <label for="kontaktName_${newIndex}">Name</label>
+          <input name="Kontakty[${newIndex}].Name" type="text" class="form-control" id="kontaktName_${newIndex}" placeholder="Název">
+          <label for="kontaktEmail_${newIndex}">Email</label>
+          <input name="Kontakty[${newIndex}].Email" type="email" class="form-control" id="kontaktEmail_${newIndex}" placeholder="Email">
+          <label for="kontaktPhone_${newIndex}">Tel</label>
+          <input name="Kontakty[${newIndex}].Phone" type="tel" class="form-control" id="kontaktPhone_${newIndex}" placeholder="Tel">
+        </div>
+      </div>
+    </div>
+  `;
+
+    // Přidat nový element na konec seznamu
+    card.innerHTML += newAccordion;
+
+    // Aktualizovat počet kontaktů v labelu
+    const kontaktLabel = document.querySelector('label[for="kontaktRow"]');
+    const kontaktCount = card.children.length;
+    kontaktLabel.innerText = `Kontakt (${kontaktCount})`;
+}
+
+// Přidat nový kontakt po kliknutí na tlačítko plus
+const addContactBtn = document.querySelector('.add-contact-btn');
+addContactBtn.addEventListener('click', addNewAccordion);
